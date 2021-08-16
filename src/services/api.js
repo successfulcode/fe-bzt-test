fae84d216034e8195c23431dfab1c4898740da68
successfulcode <sergej.mickevic@gmail.com>
2021-08-17 00:51:52 +0300

    Updated api for pagination and for get element by id

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,35 @@
-import axios from 'axios';
+import product_list from '../data/product_list.json';
 
-const url = '/data.json';
+const status = 200;
 
 export const productsApi = {
-  getAxios() {
-    return axios.get(url);
+  getProducts() {
+    console.log('getProducts');
+    return {
+      status,
+      data: product_list.splice(0, 10)
+    };
+  },
+  getProductsById(id) {
+    return {
+      status,
+      data: product_list.find((product) => product.id === id)
+    };
+  },
+  getProductsAscending() {
+    return {
+      status,
+      data: product_list
+        .sort((a, b) => b.actual_price - a.actual_price)
+        .splice(0, 5)
+    };
+  },
+  getProductsDescending() {
+    return {
+      status,
+      data: product_list
+        .sort((a, b) => a.actual_price - b.actual_price)
+        .splice(0, 5)
+    };
   }
 };
